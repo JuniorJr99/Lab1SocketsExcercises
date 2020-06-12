18d2f1572210cbbab66ffe4368d739d2a43d31f7
JuniorJr99 <66688707+JuniorJr99@users.noreply.github.com>
2020-06-11 19:34:35 -0600

    Modificaciones a la clase Server

---
+++ Lab1SocketsExcercises/src/main/java/Excercise_4/Server.java
@@ -0,0 +1,165 @@
+package Excercise_4;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class Server extends javax.swing.JFrame {
+
+    private DataOutputStream output; // output stream to client
+    private DataInputStream input; // input stream from client
+    private ServerSocket server; // server socket
+    private Socket connection; // connection to client
+    private final int PORT = 12345;
+
+    public Server() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        send = new javax.swing.JButton();
+        textField = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        textArea = new javax.swing.JTextArea();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Server");
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosed(java.awt.event.WindowEvent evt) {
+                formWindowClosed(evt);
+            }
+        });
+
+        send.setText("Send");
+        send.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                sendActionPerformed(evt);
+            }
+        });
+
+        textArea.setColumns(20);
+        textArea.setRows(5);
+        jScrollPane1.setViewportView(textArea);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 338, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(textField)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(send)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 266, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(textField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(send))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public JTextArea getTextArea() {
+        return textArea;
+    }
+
+    public void setTextArea(JTextArea textArea) {
+        this.textArea = textArea;
+    }
+
+    public JTextField getTextField() {
+        return textField;
+    }
+
+    public void runServer() {
+        try {
+            server = new ServerSocket(PORT);
+            waitForConnection();
+            getStreams();
+            processConnection();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void waitForConnection() throws IOException {
+        System.out.println("Waiting for connection...\n");
+        connection = server.accept();
+        System.out.println("Connection received from: " + connection.getInetAddress().getHostName());
+    }
+
+    private void getStreams() throws IOException {
+        output = new DataOutputStream(connection.getOutputStream());
+        output.flush();
+        input = new DataInputStream(connection.getInputStream());
+    }
+
+    private void processConnection() throws IOException {
+
+        while (true) {
+            String recievedText = input.readUTF();
+            textArea.setText(textArea.getText() + "Client -> " + recievedText + "\n");
+        }
+
+    }
+
+    private void closeConnection() {
+        System.out.println("\nTerminating connection");
+        try {
+            output.close();
+            input.close();
+            connection.close();
+            server.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void sendActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sendActionPerformed
+        try {
+            String textToSend = textField.getText().trim();
+            textArea.setText(textArea.getText() + "Server ->" + textToSend + "\n");
+            output.writeUTF(textToSend);
+            textField.setText("");
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_sendActionPerformed
+
+    private void formWindowClosed(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosed
+        closeConnection();
+    }//GEN-LAST:event_formWindowClosed
+
+    public static void main(String args[]) {
+        Server server = new Server();
+        server.setVisible(true);
+        server.runServer();
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton send;
+    private javax.swing.JTextArea textArea;
+    private javax.swing.JTextField textField;
+    // End of variables declaration//GEN-END:variables
+}
