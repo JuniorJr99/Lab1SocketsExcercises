e98b9a1299d753e87b7086b85977441460ab4b45
JuniorJr99 <66688707+JuniorJr99@users.noreply.github.com>
2020-06-11 19:33:46 -0600

    Modificaciones a la clase Server

---
+++ Lab1SocketsExcercises/src/main/java/Excercise_3/Server.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Excercise_3;
+
+import Excercise_3.ServerThread;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private Socket connection; // connection to client
+    private ServerSocket server; // server socket
+    private final int PORT = 12345;
+    private Socket[] players = new Socket[2];
+    GameManager manager = new GameManager();
+
+    // set up and run server
+    public void runServer() {
+        try {
+            server = new ServerSocket(PORT); // create ServerSocket
+            int count = 0;
+            while (count <= 2) {
+                connection = waitForConnection();
+                players[count] = connection;
+                if (count == 1) {
+                    for (int i = 0; i < 2; i++) {
+                        ServerThread serverThread = new ServerThread(players[i], manager);
+                        serverThread.start();
+                    }
+                }
+                count++;
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            closeConnection();// close connection            
+        }
+    }
+
+    // wait for connection to arrive, then display connection info
+    private Socket waitForConnection() throws IOException {
+        System.out.println("Waiting for connection...\n");
+        connection = server.accept(); // allow server to accept connection
+        System.out.println("Connection received from: " + connection.getInetAddress().getHostName());
+        return connection;
+    }
+
+    // close streams and socket
+    private void closeConnection() {
+        System.out.println("\nTerminating connection");
+        try {
+            connection.close(); // close socket  
+            server.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Server().runServer();
+    }
+}
+
